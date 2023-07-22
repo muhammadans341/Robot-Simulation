6d211363566cde7a905d5a6ffd8135cbe1ff40e4
Muhammad Ans <muhammad.ans@seamless.se>
2023-07-22 21:48:11 +0500

    initial bootstrap

---
+++ robot-simulation/src/main/java/com/assignment/robotsimulation/RobotSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.robotsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotSimulationApplication.class, args);
+    }
+
+}
